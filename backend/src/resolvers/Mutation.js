75bccf7cd9d023c631bbb6fdb7c423352794fd1f
Dan Lewis-Toakley <daniel@avaaz.org>
2019-02-04 23:12:14 -0500

    added remove item client side component

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,16 +7,16 @@ const { transport, makeAnEmail } = require('../mail')
 const { hasPermissions } = require('../utils')
 
 const Mutations = {
-    async createItem(parent, args, context, info) {
-        if (!context.request.userId) {
+    async createItem(parent, args, ctx, info) {
+        if (!ctx.request.userId) {
             throw new Error('You must be logged in to create an item!')
         }
 
-        const item = await context.db.mutation.createItem({
+        const item = await ctx.db.mutation.createItem({
             data: {
                 user: {
                     connect: {
-                        id: context.request.userId
+                        id: ctx.request.userId
                     }
                 },
                 ...args
@@ -26,12 +26,12 @@ const Mutations = {
         return item
     },
 
-    updateItem(parent, args, context, info) {
+    updateItem(parent, args, ctx, info) {
         const updateData = { ...args }
 
         delete updateData.id
 
-        return context.db.mutation.updateItem({
+        return ctx.db.mutation.updateItem({
             data: updateData,
             where: {
                 id: args.id
@@ -39,27 +39,27 @@ const Mutations = {
         }, info)
     },
 
-    async deleteItem(parent, args, context, info) {
+    async deleteItem(parent, args, ctx, info) {
         const where = { id: args.id }
-        const item = await context.db.query.item({where}, `{ id title user { id }}`)
+        const item = await ctx.db.query.item({where}, `{ id title user { id }}`)
 
-        const ownsItem = item.user.id === context.request.userId
-        const hasDeletePermissions = context.request.user.permissions
+        const ownsItem = item.user.id === ctx.request.userId
+        const hasDeletePermissions = ctx.request.user.permissions
             .some(permission => ['ADMIN', 'ITEM_DELETE']
             .includes(permission))
 
         if (!ownsItem && !hasDeletePermissions) {
             throw new Error('You don\'t have permissions!')
         }
-        return context.db.mutation.deleteItem({where}, info)
+        return ctx.db.mutation.deleteItem({where}, info)
 
     },
 
 
-    async signup(parent, args, context, info) {
+    async signup(parent, args, ctx, info) {
         args.email = args.email.toLowerCase()
         const password = await bcrypt.hash(args.password, 10)
-        const user = await context.db.mutation.createUser({
+        const user = await ctx.db.mutation.createUser({
             data: {
                 ...args,
                 password,
@@ -69,15 +69,15 @@ const Mutations = {
             }
         }, info)
 
-        generateAndSetJwtToken(user.id, context)
+        generateAndSetJwtToken(user.id, ctx)
 
         return user
 
     },
 
-    async signin(parent, { email, password }, context, info) {
+    async signin(parent, { email, password }, ctx, info) {
 
-        const user = await context.db.query.user({ where: { email }})
+        const user = await ctx.db.query.user({ where: { email }})
 
         if (!user) {
             throw new Error(`No such user found for email: ${email}`)
@@ -89,18 +89,18 @@ const Mutations = {
             throw new Error('Invalid password!')
         }
 
-        generateAndSetJwtToken(user.id, context)
+        generateAndSetJwtToken(user.id, ctx)
 
         return user
     },
 
-    signout(parent, args, context, info) {
-        context.response.clearCookie('token')
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token')
         return { message: 'You are now signed out!' }
     },
 
-    async requestPasswordReset(parent, { email }, context, info) {
-        const user = await context.db.query.user({ where: { email }})
+    async requestPasswordReset(parent, { email }, ctx, info) {
+        const user = await ctx.db.query.user({ where: { email }})
 
         if (!user) {
             throw new Error(`No such user found for email: ${email}`)
@@ -110,7 +110,7 @@ const Mutations = {
         const resetToken = (await randomBytesPromisified(20)).toString('hex')
         const resetTokenExpiry = Date.now() + 3600000 //1 hour from now
 
-        const res = await context.db.mutation.updateUser({
+        const res = await ctx.db.mutation.updateUser({
             where: {email},
             data: { resetToken, resetTokenExpiry }
         })
@@ -129,12 +129,12 @@ const Mutations = {
 
     },
 
-    async resetPassword(parent, args, context, info) {
+    async resetPassword(parent, args, ctx, info) {
         if (args.password !== args.confirmPassword) {
             throw new Error('Your passwords don\'t match!')
         }
 
-        const [user] = await context.db.query.users({
+        const [user] = await ctx.db.query.users({
             where: {
                 resetToken: args.resetToken,
                 resetTokenExpiry_gte: Date.now() - 3600000,
@@ -146,7 +146,7 @@ const Mutations = {
         }
 
         const password = await bcrypt.hash(args.password, 10)
-        const updatedUser = context.db.mutation.updateUser({
+        const updatedUser = ctx.db.mutation.updateUser({
             where: { email: user.email },
             data: {
                 password,
@@ -155,20 +155,20 @@ const Mutations = {
             }
         })
 
-        generateAndSetJwtToken(updatedUser.id, context)
+        generateAndSetJwtToken(updatedUser.id, ctx)
         return updatedUser
     },
 
-    async updateUserPermissions(parent, args, context, info) {
-        if (!context.request.userId) {
+    async updateUserPermissions(parent, args, ctx, info) {
+        if (!ctx.request.userId) {
             throw new Error('You must be logged in!')
         }
 
-        const currentUser = await context.db.query.user({ where: { id: context.request.userId }}, info)
+        const currentUser = await ctx.db.query.user({ where: { id: ctx.request.userId }}, info)
 
         hasPermissions(currentUser, ['ADMIN', 'PERMISSION_UPDATE'])
 
-        return context.db.mutation.updateUser({
+        return ctx.db.mutation.updateUser({
             where: {
                 id: args.userId
             },
@@ -180,13 +180,13 @@ const Mutations = {
         }, info)
     },
 
-    async addToCart(parent, args, context, info) {
-        const { userId } = context.request
+    async addToCart(parent, args, ctx, info) {
+        const { userId } = ctx.request
         if (!userId) {
             throw new Error('You must be logged in!')
         }
 
-        const [existingCartItem] = await context.db.query.cartItems({
+        const [existingCartItem] = await ctx.db.query.cartItems({
             where: {
                 user: { id: userId },
                 item: { id: args.id }
@@ -194,14 +194,14 @@ const Mutations = {
         })
 
         if (existingCartItem) {
-             return context.db.mutation.updateCartItem({
+             return ctx.db.mutation.updateCartItem({
                  where: { id: existingCartItem.id},
                  data: { quantity: existingCartItem.quantity + 1 }
              }, info
              )
         }
 
-        return context.db.mutation.createCartItem({
+        return ctx.db.mutation.createCartItem({
             data: {
                 user: {
                     connect: { id: userId }
@@ -212,13 +212,29 @@ const Mutations = {
             }
         }, info
         )
+    },
+
+    async removeFromCart(parent, args, ctx, info) {
+        const cartItem = await ctx.db.query.cartItem({
+            where: { id: args.id }
+            }, `{ id, user { id } }`
+        )
+        if (!cartItem) throw new Error('No CartItem found!')
+
+        if (cartItem.user.id !== ctx.request.userId) {
+            throw new Error('Item is not in user\'s cart!')
+        }
+
+        return ctx.db.mutation.deleteCartItem({
+            where: { id: args.id }
+        }, info)
     }
 }
 
-function generateAndSetJwtToken(userId, context) {
+function generateAndSetJwtToken(userId, ctx) {
 
     const token = jwt.sign({ userId: userId}, process.env.APP_SECRET)
-    context.response.cookie('token', token, {
+    ctx.response.cookie('token', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
     })
